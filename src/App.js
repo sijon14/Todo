4e0917814f7701ce5702a1595a3b855da101622c
Sijon <sijonthapa@sijons-MacBook-Pro.local>
2019-06-07 14:59:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Todoinput from "./components/Todoinput";
+import TodoList from "./components/TodoList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//uuid creates a unique ID
+export default class App extends Component {
+  state = {
+    items: [],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  //this method will hande the event and change the value of item
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    };
+    console.log(newItem);
+
+    //spread operator
+    //When CLICKED on 'ADD ITEM' => spread operator adds and updates the state to newItem >
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      // Here when uuid() function is run creates new id when creating updated items to ToDolist
+      id: uuid(),
+      editItem: false
+    });
+  };
+
+  clearList = () => {
+    this.setState({
+      items: []
+    });
+  };
+
+  // this method filters ID clicked to ID not clicked on filteredItems then passes filteredItems to state.
+  handleDelete = id => {
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filteredItems
+    });
+  };
+
+  //On handle edit we want the edit to run to the ToDo Input
+  handleEdit = id => {
+    // console.log(id);
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    console.log(filteredItems);
+
+    const selectedItems = this.state.items.find(item => item.id === id);
+    console.log(selectedItems);
+
+    this.setState({
+      items: filteredItems,
+      item: selectedItems.title,
+      editItem: true,
+      //Here we would like to grab UUID we will override and and acess the new item created ID
+      id: id
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10.mx-auto col-md-8 mt-4">
+            <h3 className="text-capitalize text-center">To do Input</h3>
+            <Todoinput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Todoinput.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export default class Todoinput extends Component {
+  render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book" />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="Add a to-do item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            //JSX allows for Ternery operator here
+            className={
+              editItem
+                ? "btn btn-block btn-success mt-3"
+                : "btn btn-block btn-primary mt-3"
+            }
+          >
+            {/* //Ternery operator */}
+            {editItem ? "Edit item" : "Add item"}
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
